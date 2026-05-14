29b03ff98086e72f4c8fc15d4caf34ddd552afbd
Benjicatch <benjamin.colimard@epitech.eu>
2026-05-15 01:10:33 +0200

    feat: better start vagrant

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,15 +3,21 @@ apt-get update
 
 # Setup github for cloning repositories
 apt-get install -y git
-echo "add host"
-ssh-keyscan github.com >> ~/.ssh/known_hosts
-echo "finish add host"
+
+ssh-keyscan github.com >> /home/vagrant/.ssh/known_hosts
+
+apt-get install -y ansible
 
 # Clone Setup_pihole repository if it doesn't exist
-if [ ! -d "~/Setup_pihole/.git" ]; then
-  cd ~/
+if [ ! -d "/home/vagrant/Setup_pihole/.git" ]; then
+  cd /home/vagrant/
   rm -rf Setup_pihole
   git clone git@github.com:Benjicatch/Setup_pihole.git
   cd Setup_pihole
-  git checkout main
+  git checkout dev
 fi
+
+cd Setup_pihole
+git pull
+
+ansible-playbook -i "localhost," -c local ./ansible/playbook.yml
