12a63ca930470408f07d85fbc40d0a0c3d3caf82
Benjicatch <benjamin.colimard@epitech.eu>
2026-05-16 00:00:04 +0200

    fix: architecture

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,21 +3,24 @@ apt-get update
 
 # Setup github for cloning repositories
 apt-get install -y git
-
 ssh-keyscan github.com >> ~/.ssh/known_hosts
-
 apt-get install -y ansible
 
+REPO_DIR="/home/vagrant/Setup_pihole"
+
 # Clone Setup_pihole repository if it doesn't exist
-if [ ! -d "/home/vagrant/Setup_pihole/.git" ]; then
-  cd /home/vagrant/
-  rm -rf Setup_pihole
-  git clone git@github.com:Benjicatch/Setup_pihole.git
-  cd Setup_pihole
-  git checkout dev
+if [ ! -d "$REPO_DIR/.git" ]; then
+  rm -rf "$REPO_DIR"
+  git clone git@github.com:Benjicatch/Setup_pihole.git "$REPO_DIR"
 fi
 
-cd Setup_pihole
-git pull
+git config --global --add safe.directory "$REPO_DIR"
+
+cd "$REPO_DIR"
+
+# Always ensure we're on the dev branch and up to date
+git fetch origin
+git checkout dev
+git pull origin dev
 
 ansible-playbook -i "localhost," -c local ./ansible/playbook.yml
